41207ec208055c4132444efca54abc5431383897
noruzzamanrubel <noruzzamanrubel@gmail.com>
2021-06-17 11:19:45 +0600

    api data read function add

--- api-fetch-all.php
+++ api-fetch-all.php
@@ -1,4 +1,16 @@
 <?php
 
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
 
-?>
+include "config.php";
+
+$sql = "SELECT * FROM students";
+$result = mysqli_query($connect, $sql) or die("Connection Failed");
+
+if (mysqli_num_rows($result) > 0) {
+    $output = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    echo json_encode($output);
+} else {
+    echo json_encode(array('message' => 'No Record Found', 'status' => false));
+}
---
+++ config.php
@@ -0,0 +1,5 @@
+<?php 
+
+$connect = mysqli_connect("localhost", "root", '', "test") or die('Connection Failed');
+
+?>
