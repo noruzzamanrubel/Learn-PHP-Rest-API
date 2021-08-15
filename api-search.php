c780f34cfadf9776cbd9c3422df130cf845a6bb3
noruzzamanrubel <noruzzamanrubel@gmail.com>
2021-08-15 16:47:50 +0600

    remove unnecessery comment code

--- api-search.php
+++ api-search.php
@@ -1,21 +1,18 @@
 <?php
 
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
+header( 'Content-Type: application/json' );
+header( 'Access-Control-Allow-Origin: *' );
 
-// $data = json_decode(file_get_contents("php://input"), true);
-// $search_value = $data['search'];
-
-$search_value = isset($_GET['search']) ? $_GET['search'] : die(); 
+$search_value = isset( $_GET['search'] ) ? $_GET['search'] : die();
 
 include "config.php";
 
-$sql = "SELECT * FROM students WHERE student_name LIKE '%{$search_value}%' ";
-$result = mysqli_query($connect, $sql) or die("Connection Failed dfef");
+$sql    = "SELECT * FROM students WHERE student_name LIKE '%{$search_value}%' ";
+$result = mysqli_query( $connect, $sql ) or die( "Connection Failed dfef" );
 
-if (mysqli_num_rows($result) > 0) {
-    $output = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    echo json_encode($output);
+if ( mysqli_num_rows( $result ) > 0 ) {
+    $output = mysqli_fetch_all( $result, MYSQLI_ASSOC );
+    echo json_encode( $output );
 } else {
-    echo json_encode(array('message' => 'No Search Found', 'status' => false));
+    echo json_encode( array( 'message' => 'No Search Found', 'status' => false ) );
 }
